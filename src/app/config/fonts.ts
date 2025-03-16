4ba813465076b4b2494f46ea5b11348c3ad40160
geraldDev01 <geralddev01@gmail.com>
2025-03-15 18:57:49 -0600

    add fonts

---
+++ src/app/config/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const montserrat = Montserrat_Alternates({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
